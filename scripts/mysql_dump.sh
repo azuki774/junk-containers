f57ed7fdf1948997dba242cd2e6856a41bb675d6
azuki774 <azuki774s@gmail.com>
2023-03-01 01:34:18 +0900

    add echo comment

--- scripts/mysql_dump.sh
+++ scripts/mysql_dump.sh
@@ -11,8 +11,15 @@ set -e
 
 YYYYMM=`date +%Y%m`
 YYYYMMDD=`date +%Y%m%d`
+echo "make YYYYMM directory"
 mkdir -p "/backup/${YYYYMM}"
+
+echo "mysqldump and output sql file"
 /usr/bin/mysqldump --skip-column-statistics -u ${DB_USER} -p${DB_PASS} -h ${DB_HOST} ${DB_NAME} > "/backup/${YYYYMM}/${YYYYMMDD}.sql"
 cd /backup/${YYYYMM}
+
+echo "compress sql file"
 tar -zcvf ${YYYYMMDD}.sql.tar.gz ${YYYYMMDD}.sql
+
+echo "remove raw dump file"
 rm /backup/${YYYYMM}/${YYYYMMDD}.sql
