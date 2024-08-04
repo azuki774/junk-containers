4da4e101f3117f9c08d4db97effd85002477965b
azuki774s <azuki774s@gmail.com>
2024-08-04 12:05:04 +0900

    add s3 uploader

--- build/mysql-dump/mysql_dump.sh
+++ build/mysql-dump/mysql_dump.sh
@@ -14,20 +14,40 @@ set -e
 # DB_NAME
 # FILE_NAME
 
+# Use awscli env
+# It is the example as follows.
+
+# BUCKET_NAME # from env (ex: hoge-system-stg-bucket)
+# BUCKET_DIR # from env (ex: fetcher/moneyforward)
+# AWS_DEFAULT_REGION # from env (ex: ap-northeast-1)
+# AWS_ACCESS_KEY_ID # from env
+# AWS_SECRET_ACCESS_KEY # from env
+
+# args from env
+# --endpoint-url=${BUCKET_URL}
+# (ex: "https://s3.ap-northeast-1.wasabisys.com")
+
 YYYYMM=`date +%Y%m`
 YYYYMMDD=`date +%Y%m%d`
 
+AWS_BIN="/usr/local/bin/aws/dist/aws"
+
 FILE_DIR="/tmp/backup/"
+SRC_DIR="/tmp/backup/"
+REMOTE_DIR=${BUCKET_DIR}
+
 if [ -z "${FILE_NAME}" ]; then
+    echo "not set filename"
     FILE_NAME="${YYYYMMDD}"
-    FILE_DIR="/tmp/backup/${YYYYMM}"
+    FILE_DIR="${FILE_DIR}/${YYYYMM}"
 fi
 
 echo "make YYYYMM directory"
 mkdir -p ${FILE_DIR}
 
+function dump () {
 echo "mysqldump and output sql file"
-/usr/bin/mysqldump --single-transaction -u ${DB_USER} -p${DB_PASS} -h ${DB_HOST} ${DB_NAME} > "${FILE_DIR}/${FILE_NAME}.sql"
+mysqldump --single-transaction -u ${DB_USER} -p${DB_PASS} -h ${DB_HOST} ${DB_NAME} > "${FILE_DIR}/${FILE_NAME}.sql"
 cd ${FILE_DIR}
 
 echo "compress sql file"
@@ -35,3 +55,20 @@ tar -zcvf ${FILE_NAME}.sql.tar.gz ${FILE_NAME}.sql
 
 echo "remove raw dump file"
 rm "${FILE_DIR}/${FILE_NAME}.sql"
+}
+
+function s3_upload () {
+    echo "s3 upload start"
+    if [ -n "$BUCKET_URL" ]; then
+        ${AWS_BIN} s3 cp ${SRC_DIR} "s3://${BUCKET_NAME}/${REMOTE_DIR}" --recursive --endpoint-url="${BUCKET_URL}"
+    else
+        ${AWS_BIN} s3 cp ${SRC_DIR} "s3://${BUCKET_NAME}/${REMOTE_DIR}" --recursive
+    fi
+    echo "s3 upload complete"
+}
+
+dump
+
+if [ -n "$BUCKET_NAME" ]; then
+    s3_upload
+fi
